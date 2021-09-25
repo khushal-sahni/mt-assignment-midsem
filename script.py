898ca885de537cd8a1d01e40cc80ab396ecb3c52
Khushal Sahni <kstheking0@gmail.com>
2021-09-25 21:43:21 +0530

    pinnacle of work

--- script.py
+++ script.py
@@ -1,11 +1,20 @@
+from locale import Error
 import requests
 
 from delphin import ace
 ## function to return a sentence for word passed to it 
 
-def sentenceGenerator(word):
-    response = requests.get(f'https://lt-nlgservice.herokuapp.com/rest/english/realise?subject={word}&verb=eat&object=human')
-
+def sentenceGenerator(word, type="subject"):
+    if(type == "subject"):
+        response = requests.get(f'https://lt-nlgservice.herokuapp.com/rest/english/realise?subject={word}&verb=eat&object=human')
+    elif (type == "verb"):
+        response = requests.get(f'https://lt-nlgservice.herokuapp.com/rest/english/realise?subject=dog&verb={word}&object=human')        
+    elif (type == "object"):
+        response = requests.get(f'https://lt-nlgservice.herokuapp.com/rest/english/realise?subject=dog&verb=eat&object={word}')        
+    elif (type == "adjective"):
+        response = requests.get(f'https://lt-nlgservice.herokuapp.com/rest/english/realise?subject=dog&verb=eat&object=human&objmod={word}')        
+    else:
+        raise Error("PLease pass correct type value")
     data = response.json()
     return data['sentence']
 
@@ -14,11 +23,13 @@ def aceParser(sentence):
     response = ace.parse('erg-1214-x86-64-0.9.24.dat', sentence, executable = './ace-0.9.24/ace')
     return response['results'][0]['mrs']
 
-def doSomething(word):
-    sentence = sentenceGenerator(word)
+def doSomething(word, type="subject"):
+    sentence = sentenceGenerator(word, type)
     aceParse = aceParser(sentence)
-    length = len(word)
     wordIndex = sentence.find(word)
+    length = 0
+    while(sentence[wordIndex + length] != ' '):
+        length += 1
     pos = aceParse.find(f'<{wordIndex}:{wordIndex+length}')  # position of that <4:20> kinda thing
 
     pos1 = pos;
@@ -37,4 +48,3 @@ def doSomething(word):
     pos3 -= 1
     concept_label = aceParse[pos2 : pos3]
     print(concept_label)
-
