4a811d54f841aceec39768fcc655bd6a8b0afb3a
Khushal Sahni <kstheking0@gmail.com>
2021-09-25 21:04:24 +0530

    stuff

--- script.py
+++ script.py
@@ -12,5 +12,30 @@ def sentenceGenerator(word):
 
 def aceParser(sentence):
     response = ace.parse('erg-1214-x86-64-0.9.24.dat', sentence, executable = './ace-0.9.24/ace')
-    print(response['results'][0]['mrs'])
+    return response['results'][0]['mrs']
+
+def doSomething(word):
+    sentence = sentenceGenerator(word)
+    aceParse = aceParser(sentence)
+    length = len(word)
+    pos = aceParse.find(f'<4:{4+length}')  # position of that <4:20> kinda thing
+
+    pos1 = pos;
+    while(aceParse[pos1-1] != ' '):
+        pos1 -= 1
+    mrs = aceParse[pos1 : pos]
+    print(mrs) 
+
+    pos2 = pos
+    while(aceParse[pos2] != ' '):
+        pos2 += 1
+    pos2 += 1
+    pos3 = pos2
+    while(aceParse[pos3] != ']'):
+        pos3 += 1
+    pos3 -= 1
+    concept_label = aceParse[pos2 : pos3]
+    print(concept_label)
+
+doSomething("fuck")
 
